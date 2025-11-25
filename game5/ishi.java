0fba1a41e9e364156b6f6d70882764f3a800d946
taiga-yan <21103@hakodate.kosen-ac.jp>
2025-11-25 09:41:15 +0900

    新規キャラ追加

--- game5/ishi.java
+++ game5/ishi.java
@@ -1,19 +1,21 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import greenfoot.*;
 
 /**
  * Write a description of class ishi here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (your name) @version (a version number or a date)
  */
 public class ishi extends Actor
 {
+
+    /* (World, Actor, GreenfootImage, Greenfoot and MouseInfo)*/
+
     /**
-     * Act - do whatever the ishi wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Act - do whatever the ishi wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
+    public void act()
     {
-        // Add your action code here.
-    }    
+        /* Add your action code here.*/
+    }
 }
