7d94ac065d9d6c2dd8e72ab2e8b17d04e5265323
Kikutariichigit config --global user.email 21043@hakodate.kosen-ac.jpgit config --global user.name Kikutariichi <21043@hakodate.kosen-ac.jp>
2025-12-09 10:20:00 +0900

    RESETボタンを追加

--- game5/MyWorld.java
+++ game5/MyWorld.java
@@ -15,5 +15,12 @@ public class MyWorld extends World
     {
         super(600, 400, 1);
         addObject( new riichi(), 300, 200 );
+        prepare();
+    }
+    private void prepare()
+    {
+       
+        ResetButton resetButton = new ResetButton();
+        addObject(resetButton, 100, 50); 
     }
 }
